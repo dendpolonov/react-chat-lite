a148c2abe58f20f3141ab47623e186494484fc53
denapolonov <144207158+dendpolonov@users.noreply.github.com>
2024-03-15 22:14:22 +0200

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+
+const PORT = 5000
+
+const http = require('http').Server(app)
+const cors = require('cors')
+
+const socketIO = require('socket.io')(http, {
+        cors: {
+            orign: 'http://localhost:5173'
+        }
+})
+
+app.get('api', (req, res) => {
+    res.json({
+        message: 'Hello'
+    })
+})
+
+const users = []
+
+
+socketIO.on('connection', (socket) => {
+    console.log(`${socket.id} user connencted`)
+    socket.on('message', (data) => {    
+        socketIO.emit('response', data)
+    })
+    socket.on('typing', (data) => socket.broadcast.emit('responseTyping', data))
+    
+    socket.on('newUser', (data) => {
+        users.push(data)
+        socketIO.emit('responseNewUser', users)
+    })
+    socket.on('disconnect', () => {
+        console.log(`${socket.id} disconnect`);
+    })
+})
+
+http.listen(PORT, () => {
+    console.log('Server worning')
+})
