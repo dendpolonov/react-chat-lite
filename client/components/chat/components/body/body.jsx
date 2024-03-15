6a9ceead38d334e5433c2f54baa9544da852cfc0
denapolonov <144207158+dendpolonov@users.noreply.github.com>
2024-03-15 22:18:23 +0200

    Add files via upload

---
+++ client/components/chat/components/body/body.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styles from './styles.module.css'
+import { useNavigate } from 'react-router-dom';
+
+const Body = ({messages, status}) => {
+
+    const navigate = useNavigate()
+
+    const handleleave = () => {
+        localStorage.removeItem('user')
+        navigate('/')
+    }
+
+    return (
+        <>
+            <header className={styles.header}>
+                <button className={styles.btn} onClick={handleleave}>Покинуть чат</button>
+            </header>
+
+            <div className={styles.container}>
+                {
+                    messages.map(element => 
+                        element.name === localStorage.getItem('user') ? (
+                            <div className={styles.chats} key={element.id}>
+                                <p className={styles.senderName}>Вы</p>
+                                <div className={styles.messageSender}>
+                                    <p className={styles.colorMessages}>{element.text}</p>
+                                </div>
+                            </div>
+                        ) : (
+                            <div className={styles.chats} key={element.id}>
+                                <p className={styles.senderNameUser}>{element.name}</p>
+                                <div className={styles.messageRecipient}>
+                                    <p className={styles.colorMessages}>{element.text}</p>
+                                </div>
+                            </div>
+                        ))
+                }
+
+            <div className={styles.status}>
+                <p>{status}...</p>
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default Body;
---
+++ client/components/chat/components/sidebar/sidebar.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import styles from './styles.module.css'
+
+const Sidebar = ({socket}) => {
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        socket.on('responseNewUser', (data) => setUsers(data))
+    }, [socket, users])
+
+    const filteredList = users.filter((value, index, self) =>
+        index === self.findIndex((t) => (
+            t.user === value.user && t.socketID === value.socketID
+        ))
+    )
+
+    return (
+        <div className={styles.sidebar}>
+            <h4 className={styles.header}>Users</h4>
+            <ul className={styles.users}> 
+                {users.map(element => (
+                    <li key={element.socketID}>{element.user}</li>
+                ))}
+            </ul>
+        </div>
+
+    );
+};
+
+export default Sidebar;
